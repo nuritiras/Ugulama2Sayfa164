6f9ddf961a4a7631202719b0b89371717cb2394d
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-02-12 22:28:50 +0300

    Add project files.

---
+++ Ugulama2Sayfa164/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ugulama2Sayfa164
+{
+    partial class FormB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(231, 54);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(35, 13);
+            this.lblAdSoyad.TabIndex = 0;
+            this.lblAdSoyad.Text = "label1";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(560, 354);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 1;
+            this.txtAdSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAdSoyad_KeyPress);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(317, 192);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 23);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "button1";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // FormB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Name = "FormB";
+            this.Text = "FormB";
+            this.Load += new System.EventHandler(this.FormB_Load);
+            this.Click += new System.EventHandler(this.FormB_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
---
+++ Ugulama2Sayfa164/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Ugulama2Sayfa164
+{
+    public partial class FormB : Form
+    {
+
+
+        public FormB()
+        {
+            InitializeComponent();
+        }
+
+        private void FormB_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FormB click olayı çalışıyor.");
+        }
+
+        private void FormB_Load(object sender, EventArgs e)
+        {
+            this.Text = "Form Sınıfı Uygulama 2";
+            this.BackColor = Color.Green;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(300, 150);
+            lblAdSoyad.Text = "Adınız";
+            lblAdSoyad.Location = new Point(10, 10);
+            lblAdSoyad.Size = new Size(65, 15);
+            lblAdSoyad.ForeColor = Color.White;
+            txtAdSoyad.Location = new Point(75, 10);
+            txtAdSoyad.Size = new Size(150, 15);
+            txtAdSoyad.TabIndex = 1;
+            btnGiris.Text = "Tıkla";
+            btnGiris.Location = new Point(100, 60);
+            btnGiris.Size = new Size(100, 30);
+            btnGiris.ForeColor = Color.White;
+            btnGiris.BackColor = Color.Black;
+            btnGiris.TabIndex = 2;
+            txtAdSoyad.KeyPress += txtAdSoyad_KeyPress;
+            btnGiris.Click += btnGiris_Click;
+            this.Controls.Add(lblAdSoyad);
+            this.Controls.Add(txtAdSoyad);
+            this.Controls.Add(btnGiris);
+        }
+
+        private void txtAdSoyad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.KeyChar = Char.ToUpper(e.KeyChar);
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba " +txtAdSoyad.Text);
+        }
+    }
+}
